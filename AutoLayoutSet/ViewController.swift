8f248e651610a5e11164dbce1910112677220c6b
楷岷 張 <MIN@Min-2.local>
2017-05-18 00:04:01 +0800

    完成

---
+++ AutoLayoutSet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayoutSet
+//
+//  Created by 楷岷 張 on 2017/5/12.
+//  Copyright © 2017年 楷岷 張. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
